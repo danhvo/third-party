16708f05e234ecbfdbb0516723a1ca1283182ec5
Danh Vo <vhdanh@gmail.com>
2020-11-25 03:49:54 +0700

    Add host information

--- src/main/java/com/danh/assignment/thirdparty/ThirdPartyApplication.java
+++ src/main/java/com/danh/assignment/thirdparty/ThirdPartyApplication.java
@@ -1,13 +1,36 @@
 package com.danh.assignment.thirdparty;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping("/infor/")
+@RestController
 public class ThirdPartyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThirdPartyApplication.class, args);
+	}
+	
+	@GetMapping(produces = { "application/json" }, path = "host")
+	public String returnHost() {
+		InetAddress ip = null;
+        try {
+            ip = InetAddress.getLocalHost();
+            System.out.println("Your current IP address : " + ip);
+ 
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        
+        return Objects.toString(ip);
 	}
 
 }
--- src/main/java/com/danh/assignment/thirdparty/bootstrap/ThirdPartyLoader.java
+++ src/main/java/com/danh/assignment/thirdparty/bootstrap/ThirdPartyLoader.java
@@ -1,5 +1,8 @@
 package com.danh.assignment.thirdparty.bootstrap;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,6 +21,17 @@ public class ThirdPartyLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+    	InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
         log.info("Loading data...");
 
